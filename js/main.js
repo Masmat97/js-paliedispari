a93c49a0eef279759d818ed9e9d2f18970bef2c8
Matteo Mascio <matteo.mascio@libero.it>
2024-04-29 16:38:26 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 
     let select = prompt("pari o dispari", );
+    console.log(select)
     let number = prompt("da 1 a 5", );
-    
-  
-
+    console.log(number)
 
 const getRandomInt = ( min = 1, max = 5 ) => {
-	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
-}
-
-const arr = [ 1, 2, 3, 4, 5, ],
+    const arr = [ 1, 2, 3, 4, 5, ],
 	items = arr.length,
 	total = items - 1,
 	index = getRandomInt( 0, total );
 	
 console.log( arr[index] ); 
+	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
 
+function somma() {
+    
+}
 
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
--
+Pari e dispari:
+-chiedere all'utente se sceglie "pari" o "dispari";
+-chiedere all'utente un numero tra 1 e 5;
+-far estrarre al computer un numero compreso tra 1 e 5;
+-fare la somma dei numeri;
+-vedere se il numero che ne esce è "pari" o "dispari";
+-decretare il vincitore;
