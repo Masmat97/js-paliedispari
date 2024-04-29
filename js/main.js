451176b3b886632133ac88cff9978c4fd9acb5dc
Matteo Mascio <matteo.mascio@libero.it>
2024-04-29 17:14:35 +0200

    3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 
-    let select = prompt("pari o dispari", );
+//     let select = prompt("pari o dispari", );
+//     console.log(select)
+//     let number = prompt("da 1 a 5", );
+//     console.log(number)
+
+// const getRandomInt = ( min = 1, max = 5 ) => {
+//     const arr = [ 1, 2, 3, 4, 5, ],
+// 	items = arr.length,
+// 	total = items - 1,
+// 	index = getRandomInt( 0, total );
+	
+// console.log( arr[index] ); 
+// 	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+// }
+
+
+//Dichiarazione
+function test() {
+    
+    let select = prompt("pari o dispari" );
     console.log(select)
-    let number = prompt("da 1 a 5", );
+    let number = prompt("da 1 a 5");
     console.log(number)
 
 const getRandomInt = ( min = 1, max = 5 ) => {
@@ -14,7 +33,15 @@ console.log( arr[index] );
 	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 }
 
-function somma() {
-    
-}
 
+
+console.log("result")
+
+
+
+
+  }
+  
+  //Esecuzione
+  test();
+  
